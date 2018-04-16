a0d5e3d4474ebe9894e454cb2fae1aaa1bb5be99
Victor Boy Simamora <kampus.victor@gmail.com>
2018-04-16 17:04:14 +0700

    Hitung Pajak

---
+++ src/hitungpajak/HitungPajak.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hitungpajak;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author victo
+ */
+public class HitungPajak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Penghasilan : ");
+        int penghasilan = input.nextInt();
+        System.out.println("Status : ");
+        String status = input.next();
+        hitungPajak(penghasilan, status);
+
+    }
+
+    public static void hitungPajak(int Penghasilan, String Status) {
+        double totalPajak = 0;
+        switch (Status) {
+            case "TK0":
+                Penghasilan = Penghasilan - 36000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "TK1":
+
+                Penghasilan = Penghasilan - 39000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "TK2":
+                Penghasilan = Penghasilan - 42000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "TK3":
+                Penghasilan = Penghasilan - 45000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "K0":
+                Penghasilan = Penghasilan - 39000000;
+               if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "K1":
+                Penghasilan = Penghasilan - 42000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "K2":
+                Penghasilan = Penghasilan - 45000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            case "K3":
+                Penghasilan = Penghasilan - 48000000;
+                if (Penghasilan > 0) {
+
+                    if (Penghasilan > 50000000) {
+                        totalPajak = totalPajak + (50000000 * 0.05);
+                        if (Penghasilan > 300000000) {
+                            totalPajak = totalPajak + (250000000 * 0.05);
+                            if (Penghasilan > 800000000) {
+                                totalPajak = totalPajak + ((Penghasilan - 800000000) * 0.05);
+                            } else {
+                                totalPajak = totalPajak + ((Penghasilan - 300000000) * 0.25);
+                            }
+                        } else {
+                            totalPajak = totalPajak + ((Penghasilan - 50000000) * 0.15);
+                        }
+                    } else {
+                        totalPajak = Penghasilan * 0.05;
+                    }
+
+                } else {
+                    totalPajak = 0;
+                }
+                System.out.println("Total Pajak : " + totalPajak);
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+}
